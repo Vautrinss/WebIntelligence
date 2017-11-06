fe6b3e2f1cc04c447b699e7660207f1a45931253
vautrinss <baptistevautrin52@gmail.com>
2017-11-06 16:03:23 +0100

    In progress

--- src/main/scala/DataSchema.scala
+++ src/main/scala/DataSchema.scala
@@ -1,3 +1,26 @@
-class DataSchema (appOrSite: String, bidfloor: Double, city: String, exhange: String, impid: String, interests: String, label: Boolean, media: String, network: String, os: String, publisher: String, size: String, timestamp: String, typeI: Int, user: String) {
+import org.apache.spark.ml.feature.StringIndexer
 
+class DataSchema (
+                   appOrSite: String,
+                   bidfloor: Double,
+                   city: String,
+                   exchange: String,
+                   impid: String,
+                   interests: String,
+                   label: Boolean,
+                   media: String,
+                   network: String,
+                   os: String,
+                   publisher: String,
+                   size: Array[Long],
+                   timestamp: Long,
+                   typeI: String,
+                   user: String
+                 )
+{
+/*
+  def inputsIndexer(col: String, outCol: String) = {
+    return new StringIndexer().setInputCol(col).setOutputCol(col + "Indexer")
+  }
+  */
 }
--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -3,6 +3,11 @@ import org.apache.spark.sql.DataFrame
 
 import org.apache.spark.sql.functions.desc
 
+import org.apache.spark.ml.classification.RandomForestClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.feature.VectorAssembler
+
 
 
 
@@ -18,7 +23,28 @@ object MainApp {
     spark.sparkContext.setLogLevel("ERROR")
 
     val df: DataFrame = spark.read.json("./data-students.json")
-    df.groupBy("label", "appOrSite").count().orderBy(desc("count")).show(500, false)
+    //val df2 = df.filter("appOrSite is not null")
+
+    //tranformor to convert string to category values
+    val appOrSiteIndexer = new StringIndexer().setInputCol("appOrSite").setOutputCol("appOrSiteCat")
+    val bidfloorIndexer = new StringIndexer().setInputCol("bidfloor").setOutputCol("bidfloorCat")
+    val cityIndexer = new StringIndexer().setInputCol("city").setOutputCol("cityCat")
+    val exchangeIndexer = new StringIndexer().setInputCol("exchange").setOutputCol("exchangeIndexer").fit(df)
+    val impidIndexer = new StringIndexer().setInputCol("impid").setOutputCol("impidIndexer").fit(df)
+    //val labelIndexer = new StringIndexer().setInputCol("label").setOutputCol("labelIndexer").fit(df)
+
+
+    //val df4 = appOrSiteIndexer.transform(df)
+    //df4.show()
+
+
+    val assembler = new VectorAssembler().setInputCols(Array("appOrSiteCat",
+      "bidfloorCat", "cityCat")).setOutputCol("features")
+    //val df3 = assembler.transform(df)
+    //df3.show()
+
+    //df.groupBy("label", "appOrSite").count().orderBy(desc("count")).show()
+
 
   }
 }
