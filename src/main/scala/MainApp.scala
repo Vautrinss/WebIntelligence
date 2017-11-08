19ceb46aad8322efccd1318f55c7a93cef573dac
Jean Miquel <jean.miquel@live.fr>
2017-11-08 19:40:57 +0100

    fini

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,197 +1,219 @@
+import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
+import org.apache.spark.ml.feature.{QuantileDiscretizer, StringIndexer, VectorAssembler}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.SparkContext._
-import org.apache.spark.sql._
-
-
-import org.apache.spark.sql.functions.desc
-
-import org.apache.spark.ml.classification.RandomForestClassifier
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.ml.feature.StringIndexer
-import org.apache.spark.ml.feature.VectorAssembler
-
-import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.feature.Binarizer
-import org.apache.spark.ml.feature.VectorSlicer
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.feature.StandardScaler
-
-import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions.{col, concat_ws, udf}
 
 
 object MainApp {
 
+    /**************************/
+   /***** INDEX METHODS ******/
+  /**************************/
 
+  /**
+    * Index the string values
+    * @param dataFrame data container
+    * @param columnNames each column name of string value
+    * @return the new dataframe with the indexed string
+    */
+  def indexTypeString(dataFrame: DataFrame, columnNames: String*): DataFrame = {
+    // create a string indexer for each string column :
+    // StringIndexer encodes a string column of labels
+    // to a column of label indices. The indices are in
+    // [0, numLabels), ordered by label frequencies.
+    columnNames.map { name => {
+      new StringIndexer()
+        .setInputCol(name)
+        .setOutputCol(s"${name}Index")
+        .fit(dataFrame)
+      // apply the transformation to the dataframe
+    }}.foldLeft(dataFrame) { (dataFrame, indexer) => {
+      indexer.transform(dataFrame)
+    }}
+  }
+
+  /**
+    * Index the boolean values by converting them in double
+    * @param dataFrame data container
+    * @param columnNames each column name of boolean value
+    * @return the new dataframe with the indexed converted boolean
+    */
+  def indexTypeBool(dataFrame: DataFrame, columnNames: String*): DataFrame = {
+    // create the converter from boolean to double
+    val toBool = udf((bool: Boolean) => if (bool) 1.0 else 0.0)
+    // loop in the different columns and create a new indexed column with the conversion
+    columnNames.foldLeft(dataFrame) { (dataFrame: DataFrame, name) => {
+      dataFrame.withColumn(s"${name}Index", toBool(dataFrame(name)))
+    }}
+  }
+
+  /**
+    * Index the double values
+    * @param dataFrame data container
+    * @param numBuckets the number of wanted decimals for the double
+    * @return the new dataframe with indexed double value
+    */
+  def indexTypeDouble(dataFrame: DataFrame, numBuckets: Int): DataFrame = {
+    // QuantileDiscretizer takes a column
+    // with continuous features and outputs a column
+    // with binned categorical features. The number of bins
+    // is set by the numBuckets parameter.
+    val qd = new QuantileDiscretizer()
+      .setInputCol("bidfloor")
+      .setOutputCol("bidfloorIndex")
+      .setNumBuckets(numBuckets)
+    // add the new index values to the dataframe
+    qd.fit(dataFrame).transform(dataFrame)
+  }
+
+
+
+    /***************************/
+   /***** MODEL CREATION ******/
+  /**************************/
+
+
+  /**
+    * Train a random forest model for the future prediction
+    * @param dataFrame data container
+    */
+  def randomForestModel(dataFrame: DataFrame): Unit = {
+    // train a random forest model with the dataframe
+    val randomForestClassifier = new RandomForestClassifier()
+      .setNumTrees(10)
+      .setMaxBins(50000)
+      .setLabelCol("labelIndex")
+      .setFeaturesCol("features")
+      .setPredictionCol("prediction")
+    randomForestClassifier
+      .fit(dataFrame)
+      .write.overwrite.save("./model")
+  }
+
+
+
+    /**************************/
+   /******** PREDICTION ******/
+  /**************************/
+
+  /**
+    * Predict the ad clicks via random forest model
+    * @param dataFrame data container
+    * @return the prediction dataframe
+    */
+  def predict(dataFrame: DataFrame) : DataFrame = {
+    // create the prediction of the dataframe based on the random forest model
+    val predictions = Some(RandomForestClassificationModel.read.load("./model")).get.transform(dataFrame)
+    predictions
+  }
+
+
+  /**
+    * Clean the predictions output
+    * @param dataFrame the prediction data container
+    * @return the new cleaned prediction
+    */
+  def cleanResult(dataFrame: DataFrame) : DataFrame = {
+    // init the converter from double to boolean
+    val toBoolean = udf((double: Double) => double == 1.0)
+    // return
+    dataFrame
+      // select only the first dataframe columns + prediction
+      .select(
+      "prediction","appOrSite","bidfloor","city","exchange","impid","interests","media",
+      "network","os","timestamp","type","user")
+      // convert the prediction values from double to boolean
+      .withColumn("prediction", toBoolean(dataFrame.col("prediction")))
+      // rename the prediction column to label
+      .withColumnRenamed("prediction", "label")
+  }
+
+
+
+    /**************************/
+   /****** CSV CREATION ******/
+  /**************************/
+
+
+  /**
+    * Write a CSV file from the dataframe
+    * @param dataFrame data container
+    */
+  def createCSV(dataFrame: DataFrame): Unit = {
+    // specify the csv format and file path
+    dataFrame
+      .coalesce(1)
+      .write.format("com.databricks.spark.csv")
+      .option("header", "true")
+      .option("delimiter", ",")
+      .save("output")
+  }
+
+
+
+
+    /**************************/
+   /************** MAIN ******/
+  /**************************/
+
+  /**
+    * The MAIN class, running the algo
+    * @param args
+    */
   def main(args: Array[String]) {
 
-    val spark: SparkSession = SparkSession
+    /* initialize a spark session for the project */
+    implicit val spark: SparkSession = SparkSession
       .builder()
       .appName("WebIntelligence")
       .master("local[*]")
       .config("spark.executor.memory", "15g")
       .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
-    import spark.implicits._
 
-    case class schemaData (
-                            appOrSite: String,
-                            bidfloor: Double,
-                            city: String,
-                            exchange: String,
-                            impid: String,
-                            interests: String,
-                            label: String,
-                            media: String,
-                            network: String,
-                            os: String,
-                            publisher: String,
-                            typeI: String,
-                            user: String
-                          )
+    /* read the data and create the dataframe */
+    var df: DataFrame = spark.read.json("./data-students.json")
 
-    case class schemaData2 (
-                            appOrSite: String,
-                            bidfloor: Double,
-                            city: String,
-                            exchange: String,
-                            impid: String,
-                            interests: String,
-                            label: Boolean,
-                            media: String,
-                            network: String,
-                            os: String,
-                            publisher: String,
-                            timestamp: Long
-                           )
-
-    val df: DataFrame = spark.read.json("./data-students.json")
-    //val df2 = df.filter("appOrSite is not null")
-    df.drop("size")
-    df.drop("timestamp")
-    val mm = df.select($"label" cast "string").as[schemaData]
-
-    mm.show(10)
-    val myRDD = df.rdd
-    //myRDD.take(20).foreach(println)
-    //val myDatas2 = df.map{x => x(6)}.take(5)
-    //myDatas2.foreach(println)
-    //df.where(df("city").isNull).groupBy("label", "city").count().orderBy(desc("count")).show(500, false)
-
-    /*val myDataEssai = myRDD
-      .filter(t => t(2) != null)
-      .filter(t => t(3) != null)
-      .filter(t => t(4) != null)
-      .filter(t => t(11) != null)
-
-      .map{x => schemaData2(x(0).toString, x(1).asInstanceOf[Double], x(2).toString, x(3).toString, x(4).toString, x(5).toString, x(6).asInstanceOf[Boolean], x(7).toString, x(8).toString, x(9).toString, x(10).toString, x(11).asInstanceOf[Long])}
-    myDataEssai.take(5).foreach(println)*/
-/*
-    val myDatas = spark.read.json("./data-students.json").drop("size").drop("timestamp")
-      .filter(t => t(2) != null)
-      .filter(t => t(3) != null)
-      .filter(t => t(4) != null)
-      .filter(t => t(5) != null)
-      .filter(t => t(7) != null)
-      .filter(t => t(8) != null)
-      .filter(t => t(9) != null)
-      .filter(t => t(10) != null)
-      .filter(t => t(12) != null)
-      .map(x =>
-      schemaData(x(0).toString, x(1).asInstanceOf[Double], x(2).toString, x(3).toString, x(4).toString, x(5).toString, x(6).toString, x(7).toString, x(8).toString, x(9).toString, x(10).toString, x(11).toString, x(12).toString)).toDF
-    myDatas.take(5).foreach(println)
-
-    /* Here we have to clean datas with .filter() */
-    // Maybe try to convert label to Int or to String
-
-    //tranformor to convert string to category values
-    val appOrSiteIndexer = new StringIndexer().setInputCol("appOrSite").setOutputCol("appOrSiteCat")
-    val bidfloorIndexer = new StringIndexer().setInputCol("bidfloor").setOutputCol("bidfloorCat")
-    val cityIndexer = new StringIndexer().setInputCol("city").setOutputCol("cityCat")
-    val exchangeIndexer = new StringIndexer().setInputCol("exchange").setOutputCol("exchangeCat")
-    val impidIndexer = new StringIndexer().setInputCol("impid").setOutputCol("impidCat")
-    val interestsIndexer = new StringIndexer().setInputCol("interests").setOutputCol("interestsCat")
-    val mediaIndexer = new StringIndexer().setInputCol("media").setOutputCol("mediaCat")
-    val networkIndexer = new StringIndexer().setInputCol("network").setOutputCol("networkCat")
-    val osIndexer = new StringIndexer().setInputCol("os").setOutputCol("osCat")
-    val publisherIndexer = new StringIndexer().setInputCol("publisher").setOutputCol("publisherCat")
-    val typeIndexer = new StringIndexer().setInputCol("type").setOutputCol("typeCat")
-    val userIndexer = new StringIndexer().setInputCol("user").setOutputCol("userCat")
+    /* fill the empty field with "null" value */
+    df = df.na.fill("null", Seq("exchange"))
+    df = df.na.fill("null", Seq("interests"))
+    df = df.na.fill("null", Seq("os"))
+    df = df.na.fill("null", Seq("publisher"))
+    df = df.na.fill("null", Seq("type"))
+    df = df.na.fill(0.0, Seq("bidfloor"))
 
 
-    //val df4 = appOrSiteIndexer.transform(df)
-    //df4.show()
 
 
-    val assembler = new VectorAssembler().setInputCols(Array(
-      "appOrSiteCat",
-      "bidfloorCat",
-      "cityCat",
-      "exchangeCat",
-      "impidCat",
-      "interestsCat",
-      "mediaCat",
-      "networkCat",
-      "osCat",
-      "publisherCat",
-      "typeCat",
-      "userCat"
-    )).setOutputCol("rawFeatures")
+    //-------------INDEX--------------
+    df = indexTypeString(df, "type", "exchange", "os", "interests")
+    df = indexTypeBool(df, "label")
+    df = indexTypeDouble(df, 2)
 
 
-    //vestor slicer
-    val slicer = new VectorSlicer().setInputCol("rawFeatures").setOutputCol("slicedfeatures").setNames(Array(
-      "appOrSiteCat",
-      "bidfloorCat",
-      "cityCat",
-      "exchangeCat",
-      "impidCat",
-      "interestsCat",
-      "mediaCat",
-      "networkCat",
-      "osCat",
-      "publisherCat",
-      "typeCat",
-      "userCat"))
-    //scale the features
-    val scaler = new StandardScaler().setInputCol("slicedfeatures").setOutputCol("features").setWithStd(true).setWithMean(true)
-
-    //labels for binary classifier
-    val binarizerClassifier = new Binarizer().setInputCol("label").setOutputCol("binaryLabel").setThreshold(15.0)
-
-    //logistic regression
-    val lr = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8).setLabelCol("binaryLabel").setFeaturesCol("features")
+    //-------------CONVERT------------
+    // add features as a base for the classification
+    // we focus on the type, exhange, OS and the interests
+    df = new VectorAssembler()
+      .setInputCols(Array("typeIndex", "exchangeIndex", "osIndex", "interestsIndex"))
+      .setOutputCol("features")
+      .transform(df)
 
 
-    // Chain indexers and tree in a Pipeline
-    val lrPipeline = new Pipeline().setStages(Array(
-      appOrSiteIndexer,
-      bidfloorIndexer,
-      cityIndexer,
-      exchangeIndexer,
-      impidIndexer,
-      interestsIndexer,
-      mediaIndexer,
-      networkIndexer,
-      osIndexer,
-      publisherIndexer,
-      typeIndexer,
-      userIndexer,
-      assembler, slicer, scaler, binarizerClassifier, lr))
-    // Train model.
-    val lrModel = lrPipeline.fit(myDatas)
-    // Make predictions.
-    val lrPredictions = lrModel.transform(df)
-    // Select example rows to display.
-    lrPredictions.select("prediction", "binaryLabel", "features").show(20)
+    //--------CREATE THE MODEL--------
+    randomForestModel(df)
 
 
-    //val df3 = assembler.transform(df)
-    //df3.show()
+    //----------PREDICT+CLEANING------
+    df = predict(df)
+    df = cleanResult(df)
 
-    //df.groupBy("label", "appOrSite").count().orderBy(desc("count")).show()
 
-*/
+    //----------CSV-------------------
+    createCSV(df)
+
+
+
   }
 }
-
